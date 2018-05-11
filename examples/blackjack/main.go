16b6bdd37eafb2e15855190c2dee1e6063c31880
Zack Patrick <zpatrick319@Hotmail.com>
2018-05-11 14:42:41 -0700

    update blackbjack example

--- examples/blackjack/main.go
+++ examples/blackjack/main.go
@@ -1,27 +1,62 @@
 package main
 
 import (
-	"fmt"
-
 	cards "github.com/zpatrick/go-cards"
 )
 
-func main() {
-	for {
-		deck := cards.NewDeck(cards.Suits(), cards.Ranks(), BlackjackValues)
-		deck.Shuffle()
+func BlackjackValues(s cards.Suit, r cards.Rank) int {
+	switch r {
+	case cards.Ace:
+		return 1
+	case cards.Two:
+		return 2
+	case cards.Three:
+		return 3
+	case cards.Four:
+		return 4
+	case cards.Five:
+		return 5
+	case cards.Six:
+		return 6
+	case cards.Seven:
+		return 7
+	case cards.Eight:
+		return 8
+	case cards.Nine:
+		return 9
+	case cards.Ten, cards.Jack, cards.Queen, cards.King:
+		return 10
+	default:
+		return 0
+	}
+}
 
-		humanHand := []cards.Card{deck.Deal(), deck.Deal()}
-		dealerHand := []cards.Card{deck.Deal(), deck.Deal()}
-
-		// todo: hit/stay logic here
-
-		switch {
-		case score(humanHand) > 21:
-			fmt.Println("Player busts!")
-		case score(dealerHand) > 21:
-			fmt.Println("Dealer busts!")
+func score(hand []cards.Card) int {
+	var numAces, score int
+	for _, card := range hand {
+		if card.Rank == cards.Ace {
+			numAces++
+			continue
 		}
 
+		score += card.Value
 	}
+
+	if numAces == 0 {
+		return score
+	}
+
+	return 0
+}
+
+func main() {
+	deck := cards.NewDeck(cards.Suits(), cards.Ranks(), BlackjackValues)
+	deck.Shuffle()
+
+	playerHand := []cards.Card{deck.Deal(), deck.Deal()}
+	dealerHand := []cards.Card{deck.Deal(), deck.Deal()}
+
+	// todo: hit/stay logic here
+
+
 }
--- examples/blackjack/terminal.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import tm "github.com/buger/goterm"
-
-func Clear() {
-	tm.Clear()
-	tm.MoveCursor(1, 1)
-	tm.Flush()
-}
